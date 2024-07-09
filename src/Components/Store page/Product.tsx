4fa0921074104660f88673c6d8c96b0246db6cca
zhi_jun <liangzhijun1216@gmail.com>
2024-07-09 16:10:12 -0400

    added another slide show in the store page

--- src/Components/Store page/Product.tsx
+++ src/Components/Store page/Product.tsx
@@ -4,6 +4,10 @@ import Slide_one from "./Slide_show/Slide_one.tsx";
 import Slide_two from "./Slide_show/Slide_two.tsx";
 import Slide_three from "./Slide_show/Slide_three.tsx";
 import Slide_four from "./Slide_show/Slide_four.tsx";
+import Slide_five from "./Slide_show_two/Slide_one.tsx";
+import Slide_six from "./Slide_show_two/Slide_two.tsx";
+import Slide_seven from "./Slide_show_two/Slide_three.tsx";
+import Slide_eight from "./Slide_show_two/Slide_four.tsx";
 
 const Product = () => {
   const slides = [
@@ -11,6 +15,10 @@ const Product = () => {
     <Slide_two />,
     <Slide_three />,
     <Slide_four />,
+    <Slide_five />,
+    <Slide_six />,
+    <Slide_seven />,
+    <Slide_eight />,
   ];
   const [currentSlide, setCurrentSlide] = useState(0);
 
@@ -31,59 +39,125 @@ const Product = () => {
   }, []);
 
   return (
-    <div className="flex">
-      <div className="flex flex-col justify-center w-full xl:px-72 md:px-10 xl:gap-[24px] px-10 md:gap-[20px] mt-10">
-        <p className="text-white text-[32px] md:text-[52px] xl:text-[48px] font-Sora font-bold w-full text-left">
-          Featured <span className="text-[#F9C80E]"> Products </span>
-        </p>
+    <>
+      <div className="flex">
+        <div className="flex flex-col justify-center w-full xl:px-72 md:px-10 xl:gap-[24px] px-10 md:gap-[20px] mt-10">
+          <p className="text-white text-nowrap text-[28px] md:text-[52px] xl:text-[32px] font-Sora font-bold w-full text-left">
+            Featured <span className="text-[#F9C80E]"> Products </span>
+          </p>
 
-        {/* Desktop */}
-        <div className="hidden md:flex flex-row gap-[32px] w-full max-w-[100vw] overflow-x-scroll no-scrollbar ">
-          <div className="transform transition duration-300 ">
-            <Slide_one />
-          </div>
-          <div>
-            <Slide_two />
-          </div>
-          <div>
-            <Slide_three />
-          </div>
-          <div>
-            <Slide_four />
-          </div>
-        </div>
-
-        {/* Mobile */}
-        <div className="md:hidden mt-20 slideshow-container">
-          {slides.map((slide, index) => (
-            <div
-              key={index}
-              className={`mySlides ${index === currentSlide ? "fade" : ""}`}
-              style={{ display: index === currentSlide ? "block" : "none" }}
-            >
-              {slide}
+          {/* Desktop */}
+          <div className="hidden md:flex flex-row gap-[32px] w-full max-w-[100vw] overflow-x-scroll no-scrollbar ">
+            <div className="transform transition duration-300 ">
+              <Slide_one />
             </div>
-          ))}
+            <div>
+              <Slide_two />
+            </div>
+            <div>
+              <Slide_three />
+            </div>
+            <div>
+              <Slide_four />
+            </div>
+          </div>
 
-          <a className="prev" onClick={prevSlide}>
-            &#10094;
-          </a>
-          <a className="next" onClick={nextSlide}>
-            &#10095;
-          </a>
-
-          <div className="dots flex justify-center mt-10">
-            {slides.map((_, index) => (
-              <span
+          {/* Mobile */}
+          <div className="md:hidden mt-20 slideshow-container">
+            {slides.map((slide, index) => (
+              <div
                 key={index}
-                className={`dot ${index === currentSlide ? "active" : ""}`}
-                onClick={() => setCurrentSlide(index)}
-              ></span>
+                className={`mySlides ${index === currentSlide ? "fade" : ""}`}
+                style={{ display: index === currentSlide ? "block" : "none" }}
+              >
+                {slide}
+              </div>
             ))}
+
+            <a className="prev" onClick={prevSlide}>
+              &#10094;
+            </a>
+            <a className="next" onClick={nextSlide}>
+              &#10095;
+            </a>
+
+            <div className="dots flex justify-center mt-10">
+              {slides.map((_, index) => (
+                <span
+                  key={index}
+                  className={`dot ${index === currentSlide ? "active" : ""}`}
+                  onClick={() => setCurrentSlide(index)}
+                ></span>
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+
+      <div className="flex">
+        <div className="flex flex-col justify-center w-full xl:px-72 md:px-10 xl:gap-[24px] px-10 md:gap-[20px] mt-10">
+          <div className="flex md:flex-row flex-col items-center">
+            {" "}
+            
+            <p className="text-white w-[335px] text-[28px] md:text-[52px] xl:text-[32px] font-Inter font-bold md:w-full text-left">
+              Dungeons & Dragons{" "}
+              <span className="text-[#F9C80E]"> Best Sellers </span>
+            </p>
+            <div className="flex justify-center flex-none w-[335px] md:w-[100px] h-[40px] border-[0.5px] border-[#F9C80E] hover:bg-[#F9C80E] p-1 rounded-[8px]">
+              <a className="font-Inter font-medium text-[18px] hover:text-black text-white">
+                View all
+              </a>
+            </div>
+          </div>
+
+          {/* Desktop */}
+          <div className="hidden md:flex flex-row gap-[32px] w-full max-w-[100vw] overflow-x-scroll no-scrollbar ">
+            <div className="transform transition duration-300 ">
+              <Slide_five />
+            </div>
+            <div>
+              <Slide_six />
+            </div>
+            <div>
+              <Slide_seven />
+            </div>
+            <div>
+              <Slide_eight />
+            </div>
+          </div>
+
+          {/* Mobile */}
+          <div className="md:hidden mt-20 slideshow-container">
+            {slides.map((slide, index) => (
+              <div
+                key={index}
+                className={`mySlides ${index === currentSlide ? "fade" : ""}`}
+                style={{ display: index === currentSlide ? "block" : "none" }}
+              >
+                {slide}
+              </div>
+            ))}
+
+            <a className="prev" onClick={prevSlide}>
+              &#10094;
+            </a>
+            <a className="next" onClick={nextSlide}>
+              &#10095;
+            </a>
+
+            <div className="dots flex justify-center mt-10">
+              {slides.map((_, index) => (
+                <span
+                  key={index}
+                  className={`dot ${index === currentSlide ? "active" : ""}`}
+                  onClick={() => setCurrentSlide(index)}
+                ></span>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
---
+++ src/Components/Store page/Slide_show_two/Slide_one.tsx
@@ -0,0 +1,127 @@
+import React, { useState } from "react";
+const Featured = [
+    {
+      pic: "/image/pic_five.svg",
+      text_one: "Pendragon Core Rut...",
+      text_two: "Three years ago, Xiao Yan, who had shown...",
+      text_three: "$24.99",
+      text_four: "Games Adventures",
+      text_five: "Long Campaigns",
+    },
+    
+    
+    
+  ];
+  
+const Slide_one = () => {
+    
+  return (
+    <>
+      <div className="hidden md:flex flex-row justify-center gap-[32px] max-w-[100vw]">
+        {Featured.map((item, index) => (
+          <div
+            key={index}
+            className="relative w-[306px] h-[439px] rounded-[10px] border border-transparent hover:border-[#FAD33E]"
+            style={{
+              backgroundImage:
+                "linear-gradient(245deg, #0D002C, rgba(255, 255, 255, 0), rgba(92, 35, 203, 0.2))",
+            }}
+          >
+            <div
+              className="absolute inset-0 rounded"
+              style={{
+                background: "inherit",
+                borderBottom: "1px solid transparent",
+                borderTop: "1px solid transparent",
+                borderImage:
+                  "linear-gradient(to right, #7b16e600, #7A16E6, #7b16e600) 1",
+                zIndex: -1,
+              }}
+            ></div>
+            <div className="relative w-full h-full flex flex-col justify-center p-4">
+              <img
+                src={item.pic}
+                alt={`Featured ${index + 1}`}
+                className="w-full h-[60%] object-contain"
+              />
+              <p className="text-white mt-4 font-Sora text-[18px] font-bold">
+                {item.text_one}
+              </p>
+              <p className="text-[#A5A5AB] mt-2 font-Inter text-[14px]">
+                {item.text_two}
+              </p>
+              <p className="text-white mt-2 font-Inter font-semibold text-[24px]">
+                {item.text_three}
+                <span className="text-[12px]"> USD</span>
+              </p>
+              <div className="flex flex-row gap-10 text-nowrap">
+                <p className="text-[#A5A5AB] mt-2 font-Inter text-[14px]">
+                  {item.text_four}
+                </p>
+                <p className="text-[#A5A5AB] mt-2 font-Inter text-[14px]">
+                  {item.text_five}
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="md:hidden slideshow-container">
+        {Featured.map((item, index) => (
+          <div
+            key={index}
+           
+          >
+            <div
+              className="relative w-[306px] h-[439px] rounded-[10px] border border-transparent hover:border-[#FAD33E]"
+              style={{
+                backgroundImage:
+                  "linear-gradient(245deg, #0D002C, rgba(255, 255, 255, 0), rgba(92, 35, 203, 0.2))",
+              }}
+            >
+              <div
+                className="absolute inset-0 rounded"
+                style={{
+                  background: "inherit",
+                  borderBottom: "1px solid transparent",
+                  borderTop: "1px solid transparent",
+                  borderImage:
+                    "linear-gradient(to right, #7b16e600, #7A16E6, #7b16e600) 1",
+                  zIndex: -1,
+                }}
+              ></div>
+              <div className="relative w-full h-full flex flex-col justify-center p-4">
+                <img
+                  src={item.pic}
+                  alt={`Featured ${index + 1}`}
+                  className="w-full h-[60%] object-contain"
+                />
+                <p className="text-white mt-4 font-Sora text-[18px] font-bold">
+                  {item.text_one}
+                </p>
+                <p className="text-[#A5A5AB] mt-2 font-Inter text-[14px]">
+                  {item.text_two}
+                </p>
+                <p className="text-white mt-2 font-Inter font-semibold text-[24px]">
+                  {item.text_three}
+                  <span className="text-[12px]"> USD</span>
+                </p>
+                <div className="flex flex-row gap-10 text-nowrap">
+                  <p className="text-[#A5A5AB] mt-2 font-Inter text-[14px]">
+                    {item.text_four}
+                  </p>
+                  <p className="text-[#A5A5AB] mt-2 font-Inter text-[14px]">
+                    {item.text_five}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+       
+      </div>
+    </>
+  );
+};
+
+export default Slide_one;
