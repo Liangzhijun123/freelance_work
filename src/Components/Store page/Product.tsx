d09bc4513a4bc5b788117ea90b9748f111510968
zhi_jun <liangzhijun1216@gmail.com>
2024-07-11 12:57:21 -0400

    added store rows

--- src/Components/Store page/Product.tsx
+++ src/Components/Store page/Product.tsx
@@ -47,8 +47,8 @@ const Product = () => {
           </p>
 
           {/* Desktop */}
-          <div className="hidden md:flex flex-row gap-[32px] w-full max-w-[100vw] overflow-x-scroll no-scrollbar ">
-            <div className="transform transition duration-300 ">
+          <div className="hidden md:flex flex-row gap-[32px] w-full max-w-[100vw] overflow-x-scroll no-scrollbar">
+            <div className="transform transition duration-300">
               <Slide_one />
             </div>
             <div>
@@ -95,10 +95,8 @@ const Product = () => {
       </div>
 
       <div className="flex">
-        <div className="flex flex-col justify-center w-full xl:px-72 md:px-10 xl:gap-[24px] px-10 md:gap-[20px] mt-10">
+        <div className="flex flex-col justify-center w-full xl:px-72 md:px-10 xl:gap-[24px] px-10 md:gap-[20px] mt-20">
           <div className="flex md:flex-row flex-col items-center">
-            {" "}
-            
             <p className="text-white w-[335px] text-[28px] md:text-[52px] xl:text-[32px] font-Inter font-bold md:w-full text-left">
               Dungeons & Dragons{" "}
               <span className="text-[#F9C80E]"> Best Sellers </span>
@@ -111,8 +109,8 @@ const Product = () => {
           </div>
 
           {/* Desktop */}
-          <div className="hidden md:flex flex-row gap-[32px] w-full max-w-[100vw] overflow-x-scroll no-scrollbar ">
-            <div className="transform transition duration-300 ">
+          <div className="hidden md:flex flex-row gap-[32px] w-full max-w-[100vw] overflow-x-scroll no-scrollbar">
+            <div className="transform transition duration-300">
               <Slide_five />
             </div>
             <div>
@@ -154,6 +152,56 @@ const Product = () => {
                 ></span>
               ))}
             </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex md:flex-row flex-col px-10 gap-[24px] mt-[80px] justify-center items-center xl:px-[300px] md:px-[100px] py-[48px]">
+        <div className="relative flex justify-center items-center w-full md:w-auto rounded-[8px] border border-transparent hover:border-[#FFC240] group">
+          <img src="/image/row_pic.svg" className="w-full md:w-[600px]" alt="Urban" />
+          <div className="absolute flex flex-col gap-2 text-center">
+            <p className="text-white text-[24px] font-bold font-Sora group-hover:text-[#F9C80E]">
+              Urban
+            </p>
+            <p className="text-white font-Inter text-[14px]">
+              Enjoy the Urban story
+            </p>
+          </div>
+        </div>
+
+        <div className="relative flex justify-center items-center w-full md:w-auto rounded-[8px] border border-transparent hover:border-[#FFC240] group">
+          <img src="/image/row_pic2.svg" className="w-full md:w-[600px]" alt="Sci-Fi" />
+          <div className="absolute flex flex-col gap-2 text-center">
+            <p className="text-white text-[24px] font-bold font-Sora group-hover:text-[#F9C80E]">
+              Sci-Fi
+            </p>
+            <p className="text-white font-Inter text-[14px]">
+              Explore the Sci-Fi Story
+            </p>
+          </div>
+        </div>
+
+        <div className="relative flex justify-center items-center w-full md:w-auto rounded-[8px] border border-transparent hover:border-[#FFC240] group">
+          <img src="/image/row_pic3.svg" className="w-full md:w-[600px]" alt="Fantasy" />
+          <div className="absolute flex flex-col gap-2 text-center">
+            <p className="text-white text-[24px] font-bold font-Sora group-hover:text-[#F9C80E]">
+              Fantasy
+            </p>
+            <p className="text-white font-Inter text-[14px]">
+              Read the Fantasy Story
+            </p>
+          </div>
+        </div>
+
+        <div className="relative flex justify-center items-center w-full md:w-auto rounded-[8px] border border-transparent hover:border-[#FFC240] group">
+          <img src="/image/row_pic4.svg" className="w-full md:w-[600px]" alt="All Story" />
+          <div className="absolute flex flex-col gap-2 text-center">
+            <p className="text-white text-[24px] font-bold font-Sora group-hover:text-[#F9C80E]">
+              All Story
+            </p>
+            <p className="text-white font-Inter text-[14px]">
+              Enjoy Our All Story
+            </p>
           </div>
         </div>
       </div>
